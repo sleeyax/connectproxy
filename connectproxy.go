9f15d836d6f220a146fa6299e68243747006bade
Sleeyax <yourd3veloper@gmail.com>
2020-07-04 17:19:47 +0200

    Fix header assignment

--- connectproxy.go
+++ connectproxy.go
@@ -245,11 +245,17 @@ func (cd *connectDialer) Dial(network, addr string) (net.Conn, error) {
 		nc.Close()
 		return nil, err
 	}
+
 	req.Close = false
+
+	if cd.config.Header != nil {
+		req.Header = cd.config.Header
+	}
+
 	if cd.haveAuth {
+		fmt.Println(cd.haveAuth)
 		req.Header.Set("Proxy-Authorization", basicAuth(cd.username, cd.password))
 	}
-	req.Header = cd.config.Header
 
 	/* Send the request */
 	err = req.Write(nc)
